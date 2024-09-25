087203bbf4919c6be387503ec7ce77828bdc2f7f
Stolas Dev <182552089+stolas-dev@users.noreply.github.com>
2024-09-25 14:47:04 -0700

    Add CI, Deployments, Tailwind

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script>
+	import '../app.css';
+	import { beforeNavigate } from '$app/navigation';
+	import { updated } from '$app/stores';
+
+	// In the event the app is open during a deployment, this will force a
+	// reload on the next navigation (poll interval via svelte.config.js).
+	// see: https://kit.svelte.dev/docs/configuration#version
+	beforeNavigate(({ willUnload, to }) => {
+		if ($updated && !willUnload && to?.url) {
+			location.href = to.url.href;
+		}
+	});
+</script>
+
+<slot></slot>
--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import * as child_process from 'node:child_process';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -16,6 +17,11 @@ const config = {
 		}),
 		paths: {
 			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		},
+		// see: https://kit.svelte.dev/docs/configuration#version
+		version: {
+			name: child_process.execSync('git rev-parse HEAD').toString().trim(),
+			pollInterval: 3000 /* ms */
 		}
 	}
 };
